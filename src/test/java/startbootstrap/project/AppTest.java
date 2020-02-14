e7829178c7367ae8e5b3e15a49f12f295e41e512
manjunath ramareddy <manjunath.ramareddy@gmail.com>
2020-02-14 14:41:46 +0530

    fourth commit

--- src/test/java/startbootstrap/project/AppTest.java
+++ src/test/java/startbootstrap/project/AppTest.java
@@ -1,6 +1,7 @@
 package startbootstrap.project;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Reporter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,7 +16,9 @@ public void test() {
 }
 
 @BeforeMethod
-public void openBrowser() {
+public void LaunchWebsite() {
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
 		System.setProperty("webdriver.chrome.driver", "D:\\selenium_drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://127.0.0.1:9090/startbootstrap/register.html");
