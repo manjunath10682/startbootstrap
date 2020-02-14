dbcbd2b14eb95a2e48127132bb77375cf199b85d
manjunath ramareddy <manjunath.ramareddy@gmail.com>
2020-02-14 14:20:36 +0530

    tjird commit

--- src/test/java/startbootstrap/project/AppTest.java
+++ src/test/java/startbootstrap/project/AppTest.java
@@ -1,16 +1,25 @@
 package startbootstrap.project;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AppTest {
 
-	public static void main(String[] args) {
+@Test
+public void test() {
+	Reporter.log("Test is successful--Manjunath");
+	System.out.println("Manjunatha");	
+}
+
+@BeforeMethod
+public void openBrowser() {
 		System.setProperty("webdriver.chrome.driver", "D:\\selenium_drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://127.0.0.1:9090/startbootstrap/register.html");
+		driver.close();
 		
 	}
-
 }
